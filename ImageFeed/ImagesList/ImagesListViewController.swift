5b5cb45ba6555804077f398e2f8cb985825a74f3
Sergey <pyzhovs.kz@yandex.kz>
2025-05-19 01:28:28 +0500

    удалил  tableView.rowHeight = 200 , больше не нужно

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -16,7 +16,6 @@ class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
     // MARK: - Setup Methods
